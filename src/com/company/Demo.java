7411d22eebd81be3fe8f0e0d6e2de73eb17d1298
taimoor <mtaimoor0167@gmail.com>
2022-09-16 18:47:31 +0500

    initial

--- src/com/company/Demo.java
+++ src/com/company/Demo.java
@@ -2,21 +2,7 @@ package com.company;
 
 public class Demo {
     public static void main(String[] args) {
-//        int limit = PrimeNumberCount.getLimit();
-        PrimeNumberCount.count(Integer.MAX_VALUE, 4);
+        int limit = PrimeNumberCount.getLimit();
+        PrimeNumberCount.count(limit, Runtime.getRuntime().availableProcessors());
     }
 }
-// 5734288 - 4
-// 5761455 - 8
-
-// 9838155
-/*
-0 - 09.19 sec
-1 - 13.94 sec
-2 - 15.87 sec
-3 - 17.41 sec
-4 - 18.54 sec
-5 - 19.39 sec
-6 - 20.04 sec
-7 - 20.51 sec
-*/
